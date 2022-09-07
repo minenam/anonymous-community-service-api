0f0a36e30efc89087742c1dc5882cc006c891a85
minenam <nhm6947@gmail.com>
2022-09-07 20:55:15 +0900

    Feat: Post API CRUD 기본 동작 구현

--- src/api/post/post.controller.ts
+++ src/api/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiOkResponse,
@@ -7,26 +7,94 @@ import {
 } from '@nestjs/swagger';
 import { CommonResponse } from '../../common/responses/common.response';
 import { PostAPIDocs } from './docs/post.docs';
+import { PasswordParams } from './dtos/passwordParams.dto';
+import { PostInputDto } from './dtos/postInput.dto';
 import { PostService } from './post.service';
 
 @ApiTags('user')
-@Controller('user')
+@Controller('api/posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   /**
-   * @code writer 작성자명
-   * @description API 설명
+   * @code writer 남혜민
+   * @description 게시글 생성 API
    *
-   * @POST ('/user')
-   *
+   * @POST ('/api/posts')
+   * @param createPostDto
    * @returns json
    */
-  @Get()
-  @ApiOperation(PostAPIDocs.usersOperation())
+  @Post()
+  @ApiOperation(PostAPIDocs.CreateOperation())
   @ApiOkResponse(CommonResponse.OkResponse())
   @ApiBadRequestResponse(CommonResponse.BadRequestException())
-  async user() {
-    return this.postService.user();
+  async createNewPost(@Body() postInputDto: PostInputDto) {
+    return await this.postService.createPost(postInputDto);
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 게시글 전체 조회 API
+   *
+   * @GET ('/api/posts')
+   * @returns json array
+   */
+  @Get()
+  @ApiOperation(PostAPIDocs.GetListOperation())
+  @ApiOkResponse(CommonResponse.OkResponse())
+  @ApiBadRequestResponse(CommonResponse.BadRequestException())
+  async getPostsList() {
+    return await this.postService.findPostsList();
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 특정 게시글 상세 조회 API
+   *
+   * @GET ('/api/posts/:id')
+   * @returns json
+   */
+  @Get(':id')
+  @ApiOperation(PostAPIDocs.GetByIdOperation())
+  @ApiOkResponse(CommonResponse.OkResponse())
+  @ApiBadRequestResponse(CommonResponse.BadRequestException())
+  async getPostDetail(@Param('id', ParseIntPipe) id: number) {
+    return await this.postService.findById(id);
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 게시글 수정 API
+   *
+   * @PUT ('/api/posts/:id')
+   * @returns json
+   */
+  @Put(':id')
+  @ApiOperation(PostAPIDocs.UpdateOperation())
+  @ApiOkResponse(CommonResponse.OkResponse())
+  @ApiBadRequestResponse(CommonResponse.BadRequestException())
+  async updatePost(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() postInputDto: PostInputDto,
+  ) {
+    return await this.postService.update(id, postInputDto);
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 게시글 직접 삭제 API
+   *
+   * @DELETE ('/api/posts/:id')
+   * @returns json
+   */
+  @Delete(':id')
+  @ApiOperation(PostAPIDocs.DeleteByIdOperation())
+  @ApiOkResponse(CommonResponse.OkResponse())
+  @ApiBadRequestResponse(CommonResponse.BadRequestException())
+  async deleteDirectPost(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('password') passwordParams: PasswordParams,
+  ) {
+    return await this.postService.deletePost(id, passwordParams);
   }
 }
--- src/api/post/post.service.ts
+++ src/api/post/post.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Body, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PostInputDto } from './dtos/postInput.dto';
+import { PostDto } from './dtos/post.dto';
+import { postsListDto } from './dtos/postsList.dto';
 import { PostEntity } from './entities/post.entity';
+import { PasswordParams } from './dtos/passwordParams.dto';
 
 @Injectable()
 export class PostService {
@@ -11,14 +15,94 @@ export class PostService {
   ) {}
 
   /**
-   * @code writer 작성자명
-   * @description 자유게시판 생성 API
+   * @code writer 남혜민
+   * @description 게시글 생성 API
    *
-   * @param
+   * @param postInputDto title, content, password
    *
-   * @return
+   * @return json
    */
-  async user() {
-    return 'user API!';
+  async createPost(postInputDto: PostInputDto) {
+    // 비밀번호 암호화
+    const newPost = this.postRepository.create(postInputDto);
+    await this.postRepository.save(newPost);
+    return newPost;
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 게시글 전체 조회 API
+   *
+   * @return json array
+   */
+  async findPostsList() {
+    const allPostList = await this.postRepository.find();
+    return allPostList.map((post) => new postsListDto(post));
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 특정 게시글 상세 조회 API
+   *
+   * @param id 게시글 id
+   *
+   * @return json
+   */
+  async findById(id: number) {
+    const post = await this.postRepository.findOne({ where: { id } });
+
+    if (!post) {
+      throw new NotFoundException('해당 게시글이 존재하지 않습니다.');
+    }
+    return new PostDto(post);
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 게시글 수정 API
+   *
+   * @param id 게시글 id
+   *
+   * @return json
+   */
+  async update(id: number, postInputDto: PostInputDto) {
+    const { password } = postInputDto;
+    // const password = 비밀번호 해독
+    // if (!previousPost) {
+    //   throw new ForbiddenException('수정 권한이 없습니다.');
+    // }
+    const previousPost = await this.postRepository.findOne({ where: { id } });
+    if (!previousPost) {
+      throw new NotFoundException('해당 게시글이 존재하지 않습니다.');
+    }
+    if (password !== previousPost.password) {
+      throw new ForbiddenException('게시글 관리 권한이 없습니다.');
+    }
+    const updatedPostResult = await this.postRepository.save({
+      ...previousPost,
+      ...postInputDto,
+    });
+    return updatedPostResult;
+  }
+
+  /**
+   * @code writer 남혜민
+   * @description 게시글 삭제 API
+   *
+   * @param id 게시글 id
+   * @param password 비밀번호
+   *
+   * @return json | error
+   */
+  async deletePost(id: number, passwordParams: PasswordParams) {
+    // const password = 비밀번호 해독
+    const post = await this.postRepository.findOne({ where: { id } });
+    if (!post) {
+      return new NotFoundException('해당 게시글이 존재하지 않습니다.');
+    }
+    if (passwordParams.password !== post.password) {
+      throw new ForbiddenException('게시글 관리 권한이 없습니다.');
+    }
+    return await this.postRepository.delete({ id });
   }
 }
